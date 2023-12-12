bb877c7f293acdeb7d7428a73f401e41fc0a6187
ArturKutlukaev <kutlukayev@bk.ru>
2023-12-12 22:02:07 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int startingScore = 100; // начальный счет
+        int startingScore = 200; // начальный счет
         int replenishment = 1100; // сумма пополнения
         int bonus = 1;            // количество бонусов
 
