657b2f69db2c84d6865d575bafc1441d5491aba8
ArturKutlukaev <kutlukayev@bk.ru>
2023-12-12 22:08:03 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,13 @@ public class Main {
         int replenishment = 1100; // сумма пополнения
         int bonus = 1;            // количество бонусов
 
-        if (replenishment > 1000) ;
-        {
+        if (replenishment > 1000) {
             bonus = 1;
         }
-        if (replenishment < 0) ;
-        {
+        if (replenishment < 0) {
             bonus = 0;
-
-            System.out.println((bonus = replenishment / 100) + "Количество бонусов");
         }
+            System.out.println((bonus = replenishment / 100) + "Количество бонусов");
+
     }
 }
